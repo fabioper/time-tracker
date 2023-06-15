fcf66b222ed10b7da990e1a58a45a2f90e88a4d0
Fábio Pereira da Silva <fabiopereira.fps@gmail.com>
2023-06-15 16:24:41 -0300

    refatora UserCard

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -3,18 +3,26 @@ import Link from "next/link"
 import Image from "next/image"
 
 export function UserCard() {
+  const user = {
+    name: "Fábio Silva",
+    picture: "https://placehold.co/400x400/png",
+  }
+
   return (
     <div className={styles.userCard}>
       <Image
-        src="https://placehold.co/400x400/png"
+        src={user.picture}
         alt=""
         width={40}
         height={40}
         className={styles.picture}
       />
+
       <div className={styles.userInfo}>
-        <strong>Fábio Silva</strong>
-        <Link href="/">Sair</Link>
+        <strong className={styles.username}>{user.name}</strong>
+        <Link href="/" className={styles.signoutLink}>
+          Sair
+        </Link>
       </div>
     </div>
   )
