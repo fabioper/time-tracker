eab0c454d12998788e50700c6c3ba99922637c2a
Fábio Pereira da Silva <fabiopereira.fps@gmail.com>
2023-06-15 15:04:41 -0300

    cria componente de header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css"
 import { type PropsWithChildren } from "react"
 import { inter } from "@/assets/fonts"
+import Header from "@/components/Header"
 
 export const metadata = {
   title: "Time Tracker",
@@ -10,7 +11,10 @@ export const metadata = {
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="pt-br" className={inter.variable}>
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
   )
 }
